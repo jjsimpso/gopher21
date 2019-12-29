0df0b8480cd97680fb711311b4b94fb5fb453815
Jonathan Simpson <jjsimpso@gmail.com>
2019-12-28 20:24:49 -0500

    Begin implementing stop words in text search. Skip stop words when processing corpus text files.
      - Checks for stop words when adding words to the search tree.
      - Still need to handle searches that include stop words before
        I can enable the feature.

--- corpus.rkt
+++ corpus.rkt
@@ -6,6 +6,7 @@
 (require racket/serialize)
 (require racket/fasl)
 (require racket/hash)
+(require racket/set)
 
 (require "trie.rkt")
 (require "file-search.rkt")
@@ -77,11 +78,25 @@
   (call-with-input-file path
     (lambda (in) (build-list in '()))))
 
+;; from https://www.textfixer.com/tutorials/common-english-words.txt
+(define stop-words (set "a" "able" "about" "across" "after" "all" "almost" "also" "am" "among" "an" "and" "any" "are""as" "at" "be" "because" "been" "but" "by" "can" "cannot" "could" "dear" "did" "do" "does" "either" "else" "ever" "every" "for" "from" "get" "got" "had" "has" "have" "he" "her" "hers" "him" "his" "how" "however" "i" "if" "in" "into" "is" "it" "its" "just" "least" "let" "like" "likely" "may" "me" "might" "most" "must" "my" "neither" "no" "nor" "not" "of" "off" "often" "on" "only" "or" "other" "our" "own" "rather" "said" "say" "says" "she" "should" "since" "so" "some" "than" "that" "the" "their" "them" "then" "there" "these" "they" "this" "tis" "to" "too" "twas" "us" "wants" "was" "we" "were" "what" "when" "where" "which" "while" "who" "whom" "why" "will" "with" "would" "yet" "you" "your"))
+
+(define (stop-word? word)
+  (define lc-word (string-downcase word))
+  (if (set-member? stop-words lc-word)
+      #t
+      #f))
+
 ;; file-index is an index to the search tree's file list
 (define (add-file-to-search-tree path file-index st)
   (define (insert-words words file-index trie)
     (cond
       [(empty? words) trie]
+      ;; remove stop word checks for now
+      #;[(stop-word? (car words))
+       (insert-words (cdr words)
+                     file-index
+                     trie)]
       [else (insert-words (cdr words)
                           file-index
                           (insert-word (car words) file-index trie))]))
