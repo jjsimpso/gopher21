80ebdacf37db9ca1cec065587a66e7b8de193d37
Jonathan Simpson <jjsimpso@gmail.com>
2021-08-22 14:48:22 -0400

    reduce html magic runtime

--- magic/html.rkt
+++ magic/html.rkt
@@ -3,18 +3,12 @@
 # HyperText Markup Language (HTML) is an SGML document type,
 # from Daniel Quinlan (quinlan@yggdrasil.com)
 # adapted to string extenstions by Anthon van der Neut <anthon@mnt.org)
-0	search/4096/cWt	\<!doctype\ html	HTML document text
+0	search/64/cWt	\<!doctype\ html	HTML document text
 
-0	search/4096/cwt	\<head\>		HTML document text
-0	search/4096/cWt	\<head\ 		HTML document text
-0	search/4096/cwt	\<title\>		HTML document text
-0	search/4096/cWt	\<title\ 		HTML document text
-0	search/4096/cwt	\<html\>		HTML document text
-0	search/4096/cWt	\<html\ 		HTML document text
-0	search/4096/cwt	\<script\> 		HTML document text
-0	search/4096/cWt	\<script\ 		HTML document text
-0	search/4096/cwt	\<style\> 		HTML document text
-0	search/4096/cWt	\<style\  		HTML document text
-0	search/4096/cwt	\<table\>		HTML document text
-0	search/4096/cWt	\<table\ 		HTML document text
-0	search/4096/cwt	\<a\ href=		HTML document text
+0	search/512/cwt	\<head\>		HTML document text
+0	search/512/cWt	\<head\ 		HTML document text
+0	search/512/cwt	\<title\>		HTML document text
+0	search/512/cWt	\<title\ 		HTML document text
+0	search/512/cwt	\<html\>		HTML document text
+0	search/512/cWt	\<html\ 		HTML document text
+0	search/512/cwt	\<a\ href=		HTML document text
